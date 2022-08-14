29140247a85be027cdc2e8fb631b859d26f572af
vmladenov <v.mladenov@hotmail.com>
2022-08-14 19:57:18 +0200

    create logout button

---
+++ client-js/src/components/header/LogoutButton.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    methods: {
+        logout() {
+            return;
+        },
+    },
+});
+</script>
+
+<template>
+    <button class="button ghost-button" v-on:click="logout">
+        <span class="icon-filled">logout</span>
+    </button>
+</template>

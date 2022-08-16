75b92e671adf7cefd1b23adb1fe1c9089c4f9b6d
vmladenov <v.mladenov@hotmail.com>
2022-08-16 09:44:56 +0200

    Add Favorite and Select Action

---
+++ client-js/src/components/files/list/actions/FavoriteAction.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+export interface FavoriteActionProps {
+    isFavorite: boolean;
+}
+
+export interface FavoriteActionEvents {
+    (event: "favorite-toggled", isFavorite: boolean): void;
+}
+
+const props = defineProps<FavoriteActionProps>();
+const emit = defineEmits<FavoriteActionEvents>();
+function onFavoriteClick() {
+    emit("favorite-toggled", props.isFavorite);
+}
+</script>
+
+<template>
+    <span
+        v-if="props.isFavorite"
+        @click="onFavoriteClick"
+        class="icon-outlined node-row-action node-row-action--visible"
+        >star</span
+    >
+    <span v-else class="icon-outlined node-row-action" @click="onFavoriteClick">star_border</span>
+</template>
---
+++ client-js/src/components/files/list/actions/SelectAction.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+export interface SelectActionProps {
+    isSelected: boolean;
+}
+
+export interface SelectActionEvents {
+    (event: "selection-toggled", isFavorite: boolean): void;
+}
+
+const props = defineProps<SelectActionProps>();
+const emit = defineEmits<SelectActionEvents>();
+function onSelectionClick() {
+    emit("selection-toggled", props.isSelected);
+}
+</script>
+
+<template>
+    <span
+        v-if="props.isSelected"
+        @click="onSelectionClick"
+        class="icon-outlined node-row-action node-row-action--visible"
+        >check_box</span
+    >
+    <span v-else class="icon-outlined node-row-action" @click="onSelectionClick">check_box_outline_blank</span>
+</template>

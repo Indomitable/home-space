d9029b85bd6cce4cbe00a77def5b2cdbca51f880
vmladenov <v.mladenov@hotmail.com>
2022-10-23 18:52:34 +0200

    chore: clean up rust code
    Signed-off-by: vmladenov <v.mladenov@hotmail.com>

--- contracts/src/favorites.rs
+++
@@ -1,7 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, PartialEq, Clone)]
-pub struct UpdateFavoriteRequest {
-    pub id: i64,
-    pub favorite: bool,
-}
--- contracts/src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub mod user;
-pub mod files;
-pub mod favorites;
